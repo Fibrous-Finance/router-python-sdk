4f8995f1d5f38d2691fa2778d00cdd2f81d77820
0xKermo <behzatc3@hotmail.com>
2024-11-21 00:33:35 +0700

    fix: downgrade version from 0.2.0 to 0.1.0 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fibrous-python',
-    version='0.2.0',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         "requests",
